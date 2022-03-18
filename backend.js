f45fe24c3da58adf6d047f64a7a5adb0c2f2cb61
codeandkey <jtst@iastate.edu>
2022-03-17 22:41:37 -0500

    implement file upload/download/tree requests

--- backend.js
+++ backend.js
@@ -1,13 +1,27 @@
 // Configuration
 const HTTP_PORT  = 8080;
 const HTTPS_PORT = 8443;
+const BUCKET = 'coms559-project-vault';
 
 // Requires
+const AWS     = require('aws-sdk');
+const bodyParser = require('body-parser');
 const express = require('express');
 const fs      = require('fs');
 const http    = require('http');
 const https   = require('https');
 const pug     = require('pug');
+
+// S3 service
+const S3 = new AWS.S3({apiVersion: '2006-03-01', region: 'us-east-1'});
+
+// test: list buckets
+S3.listBuckets((err, data) => {
+    if (err)
+        console.log('Error: ', err);
+    else
+        console.log('Success: ', data.Buckets);
+})
 
 // SSL credentials
 const privateKey = fs.readFileSync('cert/server.key', 'utf8');
@@ -22,12 +36,77 @@ const httpsServer = https.createServer(credentials, app);
 // Template engine
 app.set('view engine', 'pug');
 
-// Static routes
+// Body parsing
+app.use(express.raw({limit: '256mb', type: '*/*'}));
+
+// Web application routes
 app.use('/static', express.static('static'));
 
-// Dynamic routes
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+// REST API routes
+
+// User file upload
+app.put('/api/upload/:user/:path(*)', (req, res) => {
+    var params = {
+        Bucket: BUCKET,
+        Key: 'users/' + req.params.user + '/files/' + req.params.path,
+        Body: req.body
+    };
+
+    S3.upload(params, (err, data) => {
+        if (err)
+        {
+            console.log('Upload error: ', err);
+            res.sendStatus(500);
+        } else
+        {
+            console.log('Upload OK: ', data);
+            res.send('Thanks');
+        }
+    });
+});
+
+// User file download.
+app.get('/api/download/:user/:path(*)', (req, res) => {
+    var params = {
+        Bucket: BUCKET,
+        Key: 'users/' + req.params.user + '/files/' + req.params.path,
+    };
+
+    S3.getObject(params, (err, data) => {
+        if (err)
+        {
+            console.log('Download error: ', err);
+            res.sendStatus(500);
+        } else
+        {
+            console.log('Download OK: ', data);
+            res.send(data.Body);
+        }
+    });
+});
+
+// User directory tree.
+app.get('/api/tree/:user', (req, res) => {
+    var params = {
+        Bucket: BUCKET,
+        Prefix: 'users/' + req.params.user + '/files',
+    };
+
+    S3.listObjectsV2(params, (err, data) => {
+        if (err)
+        {
+            console.log('List error: ', err);
+            res.sendStatus(500);
+        } else
+        {
+            console.log('List OK: ', data);
+            res.send(data);
+        }
+    });
 });
 
 // Launch servers
@@ -36,5 +115,5 @@ httpServer.listen(HTTP_PORT, () => {
 });
 
 httpsServer.listen(HTTPS_PORT, () => {
-    console.log(`Listening for HTTP requests on port ${HTTPS_PORT}`);
+    console.log(`Listening for HTTPS requests on port ${HTTPS_PORT}`);
 });
